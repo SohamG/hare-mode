0c1547b8bf23918c6543cb0b5855c4a0411d0fb1
Benjamin Buccianti <benjamin@buccianti.dev>
2020-08-01 15:19:21 -0300

    basic keywords support

--- hare-mode.el
+++ hare-mode.el
@@ -1,9 +1,12 @@
-(defvar hare-keywords
+(defvar hare-mode-keywords
   '("alloc" "assert" "as" "break" "const" "continue" "def" "else" "export"
     "fn" "for" "free" "if" "is" "len" "let" "match" "return" "size" "static"
     "switch" "use" "while" "u8" "u16" "u32" "u64" "i8" "i16" "i32" "i64" "int"
     "uint" "uintptr" "f32" "f64" "bool" "char" "str" "void" "struct" "union"
     "nullable" "null" "true" "false"))
+
+(defvar hare-mode-font-lock-keywords
+  `((,(regexp-opt hare-mode-keywords 'symbols) . font-lock-keyword-face)))
 
 ;;;###autoload
 (define-derived-mode hare-mode prog-mode "Hare"
@@ -12,7 +15,8 @@
   (setq-local indent-tabs-mode t)
   (setq-local tab-width 8)
   (setq-local comment-start "/*")
-  (setq-local comment-end "*/"))
+  (setq-local comment-end "*/")
+  (setq-local font-lock-defaults '(hare-mode-font-lock-keywords)))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.ha\\'" . hare-mode))
