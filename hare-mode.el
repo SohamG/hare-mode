34dfc38024a12ef92ab517446a469404aebe29a1
Benjamin Buccianti <benjamin@buccianti.dev>
2020-08-01 14:53:54 -0300

    add hare keywords

--- hare-mode.el
+++ hare-mode.el
@@ -1,3 +1,10 @@
+(defvar hare-keywords
+  '("alloc" "assert" "as" "break" "const" "continue" "def" "else" "export"
+    "fn" "for" "free" "if" "is" "len" "let" "match" "return" "size" "static"
+    "switch" "use" "while" "u8" "u16" "u32" "u64" "i8" "i16" "i32" "i64" "int"
+    "uint" "uintptr" "f32" "f64" "bool" "char" "str" "void" "struct" "union"
+    "nullable" "null" "true" "false"))
+
 ;;;###autoload
 (define-derived-mode hare-mode prog-mode "Hare"
   "Major mode for editing `hare' files."
